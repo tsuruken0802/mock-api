fdbfae5838333c30bc36b88d84e348ffb79991fb
Kentaro Tsurumoto <tsurumotokentarou@gmail.com>
2023-02-07 22:44:23 +0900

    fix

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
 
+// Path
 const suffixApi ='/api'
+const posts = 'posts'
 
-function commonResponse(req, res) {
-    // APiで共通のレスポンスを定義する
-    const commonResponse = {
-        "result": "OK",
-        "error": { "code": "001", "message": "エラーメッセージ" },
-    }
-    res.send({
-        ...commonResponse,
-        ...res.locals.data
-    })
+// APIで共通のレスポンスを定義する
+const commonResponse = {
+    "result": "OK",
+    "error": { "code": "001", "message": "エラーメッセージ" },
 }
+
+server.get(suffixApi + '/' + posts, (req, res, next) => {
+    if ('page' in req.query) {
+      req.query._page = req.query.page;
+    }
+    if ('limit' in req.query) {
+        req.query._limit = req.query.limit;
+    }
+    next();
+});
 
 // middlewares
 server.use(middlewares)
@@ -22,7 +28,12 @@ server.use(middlewares)
 // posts
 const postsRouter = jsonServer.router('posts.json')
 server.use(suffixApi, postsRouter)
-postsRouter.render = commonResponse
+postsRouter.render = function (req, res) {
+    res.send({
+        ...commonResponse,
+        "posts": res.locals.data
+    })
+}
 
 server.listen(3000, () => {
     console.log('JSON Server is running')
