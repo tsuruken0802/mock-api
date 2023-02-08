ea7a6e2d4d315f58c52ed09baa1a36ec5f29b8b7
Kentaro Tsurumoto <tsurumotokentarou@gmail.com>
2023-02-08 22:01:16 +0900

    rewriter

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const posts = require('./posts/posts')
 
+
 server.get("/api/posts", posts.convertPaginationKey);
-server.use("/api", posts.router)
+server.use("/api/", posts.router)
+
+server.use(jsonServer.rewriter({
+    "/api/user/**": "/api/",
+}))
 
 server.listen(3000, () => {
     console.log('JSON Server is running')
